86a773023f448c127b35ebf0d4764011951b62d3
rentaotao <1191623980@qq.com>
2024-07-11 20:15:47 +0800

    0711

---
+++ console/src/main/java/com/alibaba/nacos/Nacos.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos;
+
+import com.alibaba.nacos.sys.filter.NacosTypeExcludeFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Nacos starter.
+ * <p>
+ * Use @SpringBootApplication and @ComponentScan at the same time, using CUSTOM type filter to control module enabled.
+ * NacosTypeExcludeFilter 通过启动模式，是 config 还是 naming 来确定加载配置中心的类，还是注册中心的类，还是都加载
+ * </p>
+ *
+ * @author nacos
+ */
+@SpringBootApplication
+// 不止扫描当前包下的类，扫描依赖包下的类
+@ComponentScan(basePackages = "com.alibaba.nacos", excludeFilters = {
+        @Filter(type = FilterType.CUSTOM, classes = {NacosTypeExcludeFilter.class}),
+        @Filter(type = FilterType.CUSTOM, classes = {TypeExcludeFilter.class}),
+        @Filter(type = FilterType.CUSTOM, classes = {AutoConfigurationExcludeFilter.class})})
+@ServletComponentScan
+@EnableScheduling
+public class Nacos {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nacos.class, args);
+    }
+}
+
